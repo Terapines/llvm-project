be805e1d74f9847b56fc4cc6c34e91771e429e74
yanming <ming.yan@terapines.com>
2023-03-14 17:28:36 +0800

    [RISCV] Assemble/Disassemble RVV0.71 Permutation Instructions.

--- llvm/test/MC/RISCV/rvv0p71/fmv.s
+++ llvm/test/MC/RISCV/rvv0p71/fmv.s
@@ -16,3 +16,15 @@ vfmv.v.f v8, fa0
 # CHECK-ENCODING: [0x57,0x54,0x05,0x5e]
 # CHECK-ERROR: instruction requires the following: 'V'{{.*}}'Zve32f', 'Zve64f' or 'Zve64d' (Vector Extensions for Embedded Processors){{$}}
 # CHECK-UNKNOWN: 57 54 05 5e <unknown>
+
+vfmv.f.s fa0, v4
+# CHECK-INST: vfmv.f.s fa0, v4
+# CHECK-ENCODING: [0x57,0x15,0x40,0x32]
+# CHECK-ERROR: instruction requires the following: 'V'{{.*}}'Zve32f', 'Zve64f' or 'Zve64d' (Vector Extensions for Embedded Processors){{$}}
+# CHECK-UNKNOWN: 57 15 40 32 <unknown>
+
+vfmv.s.f v8, fa0
+# CHECK-INST: vfmv.s.f v8, fa0
+# CHECK-ENCODING: [0x57,0x54,0x05,0x36]
+# CHECK-ERROR: instruction requires the following: 'V'{{.*}}'Zve32f', 'Zve64f' or 'Zve64d' (Vector Extensions for Embedded Processors){{$}}
+# CHECK-UNKNOWN: 57 54 05 36 <unknown>
--- llvm/test/MC/RISCV/rvv0p71/mv.s
+++ llvm/test/MC/RISCV/rvv0p71/mv.s
@@ -25,3 +25,15 @@ vmv.v.i v8, 15
 # CHECK-ENCODING: [0x57,0xb4,0x07,0x5e]
 # CHECK-ERROR: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' or 'Zve64x' (Vector Extensions for Embedded Processors){{$}}
 # CHECK-UNKNOWN: 57 b4 07 5e <unknown>
+
+vext.x.v a2, v4, a0
+# CHECK-INST: vext.x.v a2, v4, a0
+# CHECK-ENCODING: [0x57,0x26,0x45,0x32]
+# CHECK-ERROR: unrecognized instruction mnemonic
+# CHECK-UNKNOWN: 57 26 45 32 <unknown>
+
+vmv.s.x v8, a0
+# CHECK-INST: vmv.s.x v8, a0
+# CHECK-ENCODING: [0x57,0x64,0x05,0x36]
+# CHECK-ERROR: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' or 'Zve64x' (Vector Extensions for Embedded Processors){{$}}
+# CHECK-UNKNOWN: 57 64 05 36 <unknown>
